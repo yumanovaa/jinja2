2b55c6beca9489851a36b79a3d60d4f493132cd8
yumanovaa <nicolaeviner@gmail.com>
2018-02-13 18:27:09 +0300

    first commit

---
+++ jinja2.psm1
@@ -0,0 +1,32 @@
+﻿class Template 
+
+{
+    [string]$Template
+
+    Template ([string]$template) {
+        $this.Template = $template
+    }
+
+    [string]render ([string]$string) {
+        <#foreach ($line in $this.Template) {
+            if ($line -match "{{") {
+                Write-Host $line
+            }
+        }#>
+
+        return $this.Template -replace '{{ name }}',$string
+    }
+}
+
+function Get-Template([string]$Template) {
+    return [Template]::new($Template)
+}
+
+
+Export-ModuleMember -Function Get-Template
+
+<#public class jinja2 : Template {
+
+}
+
+$DataSource = "CRMDB"#>
