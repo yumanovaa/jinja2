c066a62af80b01f9380942d6736958c9890c6b89
yumanovaa <nicolaeviner@gmail.com>
2018-02-14 16:14:05 +0300

    Basic API  render

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# JN2PoSH
+# Jinja2 for PowerShell
+~~~~~~
+
+## Basic API Usage
+```PowerShell
+Import-Module –Name .\jinja2 -PassThru
+
+$template = Set-Template('Hello {{ name }}, I am in {{Make }} {{ Model }}!')
+$template.render(@{
+    name = "Alex"
+    Make = "Ford"
+    Model = "Mustang"
+    Color = "Red"
+    })
+```
--- jinja2.psm1
+++ jinja2.psm1
@@ -2,31 +2,35 @@
 
 {
     [string]$Template
+    [string]$BLOCK_START_STRING = '{%'
+    [string]$BLOCK_END_STRING = '%}'
+    [string]$VARIABLE_START_STRING = '{{'
+    [string]$VARIABLE_END_STRING = '}}'
+    [string]$COMMENT_START_STRING = '{#'
+    [string]$COMMENT_END_STRING = '#}'
+    #$VARIABLE_STRING = "{{.\w*.}}"
+    #$BLOCK_STRING = "{%.\w*.%}"
+    #$COMMENT_STRING = "{#.\w*.#}"
 
     Template ([string]$template) {
         $this.Template = $template
     }
 
-    [string]render ([string]$string) {
-        <#foreach ($line in $this.Template) {
-            if ($line -match "{{") {
-                Write-Host $line
+    [string]render ($dictionary) {
+        [string]$Rezult = $this.Template
+        if ($dictionary.GetType().Name -eq "Hashtable") {
+            foreach ($key in $dictionary.keys) {
+                if ($Rezult -match ($this.VARIABLE_START_STRING + '\s*' + $key  + '\s*' + $this.VARIABLE_END_STRING)){
+                    $Rezult = $Rezult -replace ($this.VARIABLE_START_STRING + '\s*' + $key  + '\s*' + $this.VARIABLE_END_STRING),($dictionary[$key])
+                }
             }
-        }#>
-
-        return $this.Template -replace '{{ name }}',$string
+        }
+        return $Rezult
     }
 }
 
-function Get-Template([string]$Template) {
+function Set-Template([string]$Template) {
     return [Template]::new($Template)
 }
 
-
-Export-ModuleMember -Function Get-Template
-
-<#public class jinja2 : Template {
-
-}
-
-$DataSource = "CRMDB"#>
+Export-ModuleMember -Function Set-Template
