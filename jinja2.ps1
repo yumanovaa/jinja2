8db324295be9639031d3d6f60f1bd0a3c685c530
yumanovaa <nicolaeviner@gmail.com>
2018-02-22 13:49:56 +0300

    Add ScriptFileInfo for PSGalleray

---
+++ jinja2.ps1
@@ -0,0 +1,46 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0.2
+
+.GUID 9f4449f8-c9a6-4f67-b02d-b4e49fa9334e
+
+.AUTHOR 'Yumanov Aleksey'
+
+.COMPANYNAME 'Flexible Systems'
+
+.COPYRIGHT '(c) 2018 Yumanov A.A. All rights reserved.'
+
+.TAGS 'jinja2'
+
+.LICENSEURI 
+
+.PROJECTURI https://github.com/yumanovaa/jinja2
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES jinja2.psm1
+
+.RELEASENOTES
+ Release 1.0.2
+ Processing String array
+ Processing Array with String array
+ Release 1.0.1
+ Basick API Fincrion
+ Processing placeholder
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ This is PowerShell module for Jinja2 Templates
+
+#> 
+Param()
+
+
